a9ff47fe65b766b84cab339bfef23c73a956242b
julek64 <juliusz644@gmail.com>
2021-05-08 15:20:27 +0200

    Add main models.

---
+++ NeuralNetwork.Core/Models/NeuralNetwork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork.Core.Models
+{
+    public class NeuralNetwork
+    {
+        public Layer[] Layers { get; set; }
+    }
+}
