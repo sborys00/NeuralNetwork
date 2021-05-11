85537ebfbd3f34554ba8759dd85dd086b3427a3c
Juliusz Zagdański <juliusz644@gmail.com>
2021-05-11 22:06:54 +0200

    Add CalculateOutput method in Neuron.

--- NeuralNetwork.Core/Models/Neuron.cs
+++ NeuralNetwork.Core/Models/Neuron.cs
@@ -9,6 +9,19 @@ namespace NeuralNetwork.Core.Models
     public class Neuron
     {
         public List<double> Weights { get; set; } = new();
-        public double Value { get; set; }
+
+        public static double CalculateOutput(double[] input, double[] enteringWeights)
+        {
+            if (input.Length != enteringWeights.Length)
+                throw new Exception("Amount of input values is different from amount of weights coming in.");
+
+            double output = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                output += input[i] * enteringWeights[i];
+            }
+
+            return output;
+        }
     }
 }
