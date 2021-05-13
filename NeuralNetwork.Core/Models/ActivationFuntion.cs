110000a28f0aa114515e62cf25ad1fc2ca44c6e2
sborys00 <16275683+sborys00@users.noreply.github.com>
2021-05-13 11:26:21 +0200

    add basic activation functions

---
+++ NeuralNetwork.Core/Models/ActivationFuntion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork.Core.Models
+{
+    public static class ActivationFuntion
+    {
+        public static double Sigmoid(double x) => 1 / (1 + Math.Exp(-x));
+        public static double Tanh(double x) => (Math.Exp(x) - Math.Exp(-x)) / (Math.Exp(x) + Math.Exp(-x));
+        public static double ReLU(double x) => Math.Max(0d, x);               
+    }
+}
