ebd2b786cd26bb8dd9422a844cce5323de6c1dd0
sborys00 <16275683+sborys00@users.noreply.github.com>
2021-05-13 11:49:11 +0200

    add InputData model

---
+++ NeuralNetwork.Core/Models/InputData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork.Core.Models
+{
+    class InputData
+    {
+        public IEnumerable<string> VariableNames;
+        public IEnumerable<IEnumerable<double>> DataSet; 
+    }
+}
