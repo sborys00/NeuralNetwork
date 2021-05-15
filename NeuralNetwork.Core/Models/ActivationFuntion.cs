1d2e7cfc83b7ddaf0d7d24859580db95c9aaf312
sborys00 <16275683+sborys00@users.noreply.github.com>
2021-05-15 17:56:57 +0200

    fix wrong function formula

--- NeuralNetwork.Core/Models/ActivationFuntion.cs
+++ NeuralNetwork.Core/Models/ActivationFuntion.cs
@@ -11,7 +11,7 @@ namespace NeuralNetwork.Core.Models
         public abstract Func<double, double> Function { get;} 
         public abstract Func<double, double> Derivative { get;} 
         public static double Sigmoid(double x) => 1 / (1 + Math.Exp(-x));
-        public static double SigmoidBipolar(double x) => (1 - Math.Exp(x)) / (1 + Math.Exp(-x));
+        public static double SigmoidBipolar(double x) => (1 - Math.Exp(-x)) / (1 + Math.Exp(-x));
         public static double Tanh(double x) => (Math.Exp(x) - Math.Exp(-x)) / (Math.Exp(x) + Math.Exp(-x));
         public static double ReLU(double x) => Math.Max(0d, x);
 
