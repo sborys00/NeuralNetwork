b032364f1d69d2d0c1014c4085d0d3fa447be41c
sborys00 <16275683+sborys00@users.noreply.github.com>
2021-05-26 15:14:12 +0200

    Add DataGrid with training examples

--- NeuralNetwork.UI/ViewModels/DataViewModel.cs
+++ NeuralNetwork.UI/ViewModels/DataViewModel.cs
@@ -8,6 +8,7 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Data;
 
 namespace NeuralNetwork.UI.ViewModels
 {
@@ -17,43 +18,25 @@ namespace NeuralNetwork.UI.ViewModels
 
         public DelegateCommand LoadFileCommand { get; set; }
 
+        private DataTable dataTable;
 
-        private ObservableCollection<TrainingDataExample> trainingExamples;
+        public DataTable DataTable
+        {
+            get { return dataTable; }
+            set { SetProperty(ref dataTable, value); }
+        }
 
-        private ObservableCollection<TrainingDataExample> testExamples;
-
-        private ObservableCollection<string> traningExamplesString;
 
         public DataViewModel(IFileReader fileReader)
         {
             _fileReader = fileReader;
             LoadFileCommand = new DelegateCommand(LoadFile);
-
-            TraningExamples = new();
-            TestExamples = new();
-        }
-        public ObservableCollection<TrainingDataExample> TraningExamples
-        {
-            get { return trainingExamples; }
-            set
-            {
-                trainingExamples = value;
-            }
-        }
-
-        public ObservableCollection<TrainingDataExample> TestExamples
-        {
-            get { return testExamples; }
-            set 
-            { 
-                testExamples = value;
-            }
         }
         public async void LoadFile()
         {
             string fileName = SelectDataFile();
-            TrainingDataset data = await _fileReader.ReadInputData(fileName, new int[] { 1 });
-            TraningExamples.AddRange(data.Dataset);
+            TrainingDataset data = await _fileReader.ReadInputData(fileName, new int[] { 3 });
+            DataTable = GenerateDataTable(data);
         }
 
         public string SelectDataFile()
@@ -69,6 +52,39 @@ namespace NeuralNetwork.UI.ViewModels
                 return dialog.FileName;
             }
             throw new FileNotFoundException("File could not be loaded");
+        }
+
+        private DataTable GenerateDataTable(TrainingDataset trainingDataset)
+        {
+            DataTable dataTable = new();
+            List<TrainingDataExample> dataset = trainingDataset.Dataset.ToList();
+            int inputCount = dataset[0].inputValues.Length;
+            int outputCount = dataset[0].expectedOutputs.Length;
+
+            dataTable.Columns.Add(new DataColumn() { ColumnName = $"Test", DataType = typeof(bool) });
+            for (int i = 0; i < inputCount + outputCount; i++)
+            {
+                if(i < inputCount)
+                    dataTable.Columns.Add(new DataColumn() { ColumnName = $"in {i}", DataType = typeof(double) });
+                else
+                    dataTable.Columns.Add(new DataColumn() { ColumnName = $"out {i - inputCount}", DataType = typeof(double) });
+            }
+
+            for (int i = 0; i < dataset.Count; i++)
+            {
+                DataRow row = dataTable.NewRow();
+                row[0] = false;
+                int j;
+                for (j = 0; j < inputCount + outputCount;  j++)
+                {
+                    if(j < inputCount)
+                        row[j + 1] = dataset[i].inputValues[j];
+                    else
+                        row[j + 1] = dataset[i].expectedOutputs[j - inputCount];
+                }
+                dataTable.Rows.Add(row);
+            }
+            return dataTable;
         }
     }
 }
