b89f49c0e0a20dec0d92ceb7ffb1dceb9d5f358c
Juliusz Zagdański <juliusz644@gmail.com>
2021-06-08 17:48:57 +0200

    Add AddNeuronToLayer method

--- NeuralNetwork.Core/Models/Network.cs
+++ NeuralNetwork.Core/Models/Network.cs
@@ -73,6 +73,15 @@ namespace NeuralNetwork.Core.Models
         }
 
         /// <summary>
+        /// Adds one neuron to hidden layer at index, where 0 is input layer and amountOfNeurons - 1 is output layer.
+        /// </summary>
+        /// <param name="index"></param>
+        public void AddNeuronToLayer(int index)
+        {
+            ChangeLayerNeuronAmount(index, Layers[index].Neurons.Count + 1);
+        }
+
+        /// <summary>
         /// Sets initial weight values in all layers using Normalized Xavier Weight Initialization
         /// </summary>
         public void InitializeWeights()
