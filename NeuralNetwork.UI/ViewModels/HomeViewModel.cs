ef587e08df6102cecde959b6f27eb1a8637e5802
sborys00 <16275683+sborys00@users.noreply.github.com>
2021-06-10 16:09:41 +0200

    Set inital HomeView

--- NeuralNetwork.UI/ViewModels/HomeViewModel.cs
+++ NeuralNetwork.UI/ViewModels/HomeViewModel.cs
@@ -1,8 +1,19 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 
 namespace NeuralNetwork.UI.ViewModels
 {
     public class HomeViewModel : BindableBase
     {
+        public HomeViewModel()
+        {
+            OpenGithubRepoCommand = new DelegateCommand<string>(OpenGithubRepo);
+        }
+        public DelegateCommand<string> OpenGithubRepoCommand { get; set; }
+
+        public void OpenGithubRepo(string url)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", url);
+        }
     }
 }
