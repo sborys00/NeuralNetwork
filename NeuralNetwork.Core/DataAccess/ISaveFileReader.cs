470811ec5c78eda4ac464f15f55faa55a4e382c9
sborys00 <16275683+sborys00@users.noreply.github.com>
2021-06-13 13:43:23 +0200

    Add SaveFileReader

---
+++ NeuralNetwork.Core/DataAccess/ISaveFileReader.cs
@@ -0,0 +1,11 @@
+﻿using NeuralNetwork.Core.Models;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork.Core.DataAccess
+{
+    public interface ISaveFileReader
+    {
+        Task<Save> ReadSave(string path);
+        Task WriteSave(string path, Save save);
+    }
+}
---
+++ NeuralNetwork.Core/DataAccess/SaveFileReader.cs
@@ -0,0 +1,36 @@
+﻿using NeuralNetwork.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork.Core.DataAccess
+{
+    public class SaveFileReader : ISaveFileReader
+    {
+        private readonly IFileSystem _fileSystem;
+
+        public SaveFileReader(IFileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+        }
+        public SaveFileReader() : this(new FileSystem()) { }
+        public async Task<Save> ReadSave(string path)
+        {
+            using StreamReader sr = _fileSystem.File.OpenText(path);
+            string json = await sr.ReadToEndAsync();
+            Save save = JsonSerializer.Deserialize<Save>(json);
+            return save;
+        }
+        public async Task WriteSave(string path, Save save)
+        {
+            using StreamWriter sw = _fileSystem.File.CreateText(path);
+            string json = JsonSerializer.Serialize(save);
+            await sw.WriteAsync(json);
+        }
+    }
+}
